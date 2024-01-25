2feb39a2a186f604c3660270d9f8fe46fc37c934
Ian Kisali <iankisali@gmail.com>
2024-01-25 15:38:10 +0300

    pyhon recap -> if else, func

--- Recap/days_to_hours.py
+++ Recap/days_to_hours.py
@@ -2,7 +2,21 @@ calc_to_units = 24
 name_of_unit = "hours"
 
 def days_to_units(num_of_days):
-    print(f"{num_of_days} days are {num_of_days * calc_to_units} {name_of_unit}")
-    print("All good")
+   return f"{num_of_days} days are {num_of_days * calc_to_units} {name_of_unit}"
 
-days_to_units(20)
+def validate_execute():
+   try:
+     user_num = int(user_input)
+     if user_num > 0:
+        calc_value = days_to_units(user_num)
+        print(calc_value)
+     elif user_num == 0:
+        print ("You entered zero!!!")
+     else:
+        print ('Entered negative number!!')
+     
+   except ValueError:
+      print("Invalid value!!")
+
+user_input = input("Enter hours to convert to days!\n")
+validate_execute()
